b85d54da2e88ce5438682202148423bbbeb1ddb3
Jiachen <jcjiang42@gmail.com>
2017-04-20 15:04:57 -0400

    finished

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(640, 480);
+}
+
+function draw() {
+    var red = random(255);
+    var green = random(255);
+    var blue = random(255);
+    stroke(255, 255, 255, 25);
+    fill((mouseX / width) * 255, (mouseY / height) * 255, 0, 100);
+    rect(mouseX,mouseY,50,50);
+}
